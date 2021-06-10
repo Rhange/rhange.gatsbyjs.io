eb65f68182b31b0d2cebf53ad92612dadb434429
rhange <rhange0511@gmail.com>
2021-06-10 17:19:29 +0900

    Updated darkmode checkbox

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,34 +1,51 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { Link } from 'gatsby';
+import { ThemeToggler } from 'gatsby-plugin-dark-mode';
 
 const Layout = ({ location, title, children }) => {
-  const rootPath = `${__PATH_PREFIX__}/`
-  const isRootPath = location.pathname === rootPath
-  let header
+  const rootPath = `${__PATH_PREFIX__}/`;
+  const isRootPath = location.pathname === rootPath;
+  let header;
 
   if (isRootPath) {
     header = (
       <h1 className="main-heading">
         <Link to="/">{title}</Link>
       </h1>
-    )
+    );
   } else {
     header = (
       <Link className="header-link-home" to="/">
         {title}
       </Link>
-    )
+    );
   }
 
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
-      <header className="global-header">{header}</header>
+      <div className="header-toggle">
+        <header className="global-header">{header}</header>
+        <ThemeToggler>
+          {({ theme, toggleTheme }) => (
+            <label className="toggleLabel">
+              <input
+                type="checkbox"
+                onChange={(e) =>
+                  toggleTheme(e.target.checked ? 'dark' : 'light')
+                }
+                checked={theme === 'dark'}
+              />{' '}
+              Dark mode
+            </label>
+          )}
+        </ThemeToggler>
+      </div>
       <main>{children}</main>
       <footer>
         <a href="https://github.com/rhange">Github</a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
